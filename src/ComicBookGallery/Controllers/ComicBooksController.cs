73945085670784fab90a35fca8f29718c4711dfa
Jeremy Juengel <jeremy.juengel@live.com>
2017-04-06 21:28:03 -0600

    added default view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,12 +10,12 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Thursday)
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
             {
                 return Redirect("/");
             }
 
-            return Content("Hello from the comic books controller!");
+            return View();
         }
     }
 }
